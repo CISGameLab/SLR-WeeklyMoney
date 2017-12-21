47119cddf992cd62a364a75c26ff272100a623b0
PachecoLuke <ltpachec@umich.edu>
2017-12-21 01:31:31 -0500

    Colliders and began balance script

---
+++ Assets/Balance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace BalanceScore
+{
+
+    public class Balance : MonoBehaviour
+    {
+        public int currBalance;
+        public int maxBalance = 200;
+        public int minBalance= -200;
+        public int itemBalance;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
